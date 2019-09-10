c2967090301cdd08515c46a2a59c7bc7d8bf7798
JohannesW-DE <admin@johannes-weinsheimer.de>
2019-09-10 09:51:29 +0200

    removed some leftover files

--- app/src/main/java/dev/weinsheimer/sportscalendar/database/TestDao.kt
+++
@@ -1,8 +0,0 @@
-package dev.weinsheimer.sportscalendar.database
-
-import androidx.lifecycle.LiveData
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-
